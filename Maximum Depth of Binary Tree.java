896a905aff6db03ebeb8d1ee9425d08a01b8e7e6
riaing <panjfang@gmail.com>
2016-10-27 21:57:11 -0400

    Update Maximum Depth of Binary Tree.java

--- Maximum Depth of Binary Tree.java
+++ Maximum Depth of Binary Tree.java
@@ -20,3 +20,34 @@ public class Solution {
         return 1 + Math.max(maxDepth(root.left) , maxDepth(root.right));
     }
 }
+
+非递归解法
+
+public int maxDepth(TreeNode root) {
+ 2     if(root == null)
+ 3         return 0;
+ 4     
+ 5     int depth = 0;
+ 6     LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+ 7     queue.add(root);
+ 8     int curNum = 1; //num of nodes left in current level
+ 9     int nextNum = 0; //num of nodes in next level
+10     while(!queue.isEmpty()){
+11         TreeNode n = queue.poll();
+12         curNum--;
+13         if(n.left!=null){
+14             queue.add(n.left);
+15             nextNum++;
+16         }
+17         if(n.right!=null){
+18             queue.add(n.right);
+19             nextNum++;
+20         }
+21         if(curNum == 0){
+22             curNum = nextNum;
+23             nextNum = 0;
+24             depth++;
+25         }
+26     }
+27     return depth;
+28 }
