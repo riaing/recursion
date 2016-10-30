8b399151afa4398694154635aabf18dbca339009
riaing <panjfang@gmail.com>
2016-10-29 23:21:43 -0400

    Update Path Sum II.java

--- Path Sum II.java
+++ Path Sum II.java
@@ -54,3 +54,46 @@ public class Solution {
         
     }
 }
+
+-------------------DFS------
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+   
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> allpaths = new ArrayList<List<Integer>>();
+        List<Integer> curpath =new ArrayList<Integer>(); 
+        search( root, sum, curpath, allpaths ); 
+        return allpaths; 
+    }
+    
+    private void search (TreeNode root, int sum, List<Integer> curpath, List<List<Integer>> allpaths){
+        if (root == null){
+            return;
+        }
+        
+        curpath.add(root.val); //add here to avoid duplicate times. 
+        if (root.left == null  && root.right == null && root.val == sum){
+            //if(root.val == sum){
+                //curpath.add(root.val);
+                allpaths.add(new ArrayList<Integer>(curpath)); 
+                //curpath.remove(curpath.size()-1);  //@
+           // }
+        }
+        else{ 
+        //curpath.add(root.val) ; 
+        search(root.left, sum-root.val, curpath, allpaths); 
+       
+        search(root.right, sum-root.val, curpath, allpaths); 
+        //curpath.remove(curpath.size() -1 ); // 用else,避免remove写两遍 //@ 
+        } 
+        curpath.remove(curpath.size() -1 ) ; //@
+    }
+}
