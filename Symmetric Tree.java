651d9fdc5c77a561e68826066cdbd9fa1d5241dd
riaing <panjfang@gmail.com>
2016-10-29 20:55:33 -0400

    Update Symmetric Tree.java

--- Symmetric Tree.java
+++ Symmetric Tree.java
@@ -44,3 +44,39 @@ public class Solution {
          return isSymmetric(q.left, p.right) && isSymmetric(q.right , p.left);
      } 
 }
+------------------------recursion------------------
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        return helper(root.left, root.right);
+    }
+    
+    private boolean helper(TreeNode left, TreeNode right){
+
+        if(left == null && right != null){
+            return false;
+        }
+        if(left != null && right == null){
+            return false; 
+        }
+        if(left == null && right == null){
+            return true; 
+        }
+        if(left.val != right.val){
+            return false; 
+        }
+        return helper(left.left, right.right) && helper(left.right, right.left); 
+        
+     } 
+}
