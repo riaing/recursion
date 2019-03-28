d87e434e33145ecd3c2aa3287f3820bfcadaa959
riaing <panjfang@gmail.com>
2019-03-28 13:49:17 -0700

    Update 101. Symmetric Tree.java

--- 101. Symmetric Tree.java
+++ 101. Symmetric Tree.java
@@ -1,5 +1,5 @@
 Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
-还可用BFS解。 
+ 
 
 For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
 
@@ -44,4 +44,51 @@ class Solution {
         && isMirror(t1.right, t2.left)
         && isMirror(t1.left, t2.right);
     }
+}
+
+----------------------非递归--------------------------------------------------------------------------------
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/**
+Two trees are a mirror reflection of each other if:
+
+Their two roots have the same value.
+The right subtree of each tree is a mirror reflection of the left subtree of the other tree.
+*/
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root.left);
+        q.offer(root.right);
+        while(!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            
+            if (t1 == null && t2 == null) {
+                continue;   
+            } // 这里很重要
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+            if (t1.val != t2.val) {
+                return false;
+            }
+            // 因为互为镜像，所以用t1.left与t2.right对比。
+            q.offer(t1.left);
+            q.offer(t2.right);
+            q.offer(t1.right);
+            q.offer(t2.left);
+        }
+        return true;
+    }    
 }
