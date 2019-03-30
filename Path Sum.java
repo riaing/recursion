9f07f8766b38970e251b4f9a166bf9e6669c4e82
riaing <panjfang@gmail.com>
2019-03-30 00:30:16 -0700

    Update Path Sum.java

--- Path Sum.java
+++ Path Sum.java
@@ -32,3 +32,49 @@ public class Solution {
         
     }
 }
+------------------用stack(iteration)来实现dfs-----------------------------------------------------------------------
+  /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/**用iteration来实现dfs的思想，需要借助stack。也是iterate每个node，如果不成，就把child放到stack里面去，那么下次循环就会用到child，
+则遵循了dfs的思想。
+Time complexity : the same as the recursion approach mathcal{O}(N).
+Space complexity : {O}(N) since in the worst case, when the tree is completely unbalanced, e.g. each node has only one 
+child node, we would keep all NN nodes in the stack. But in the best case (the tree is balanced), the height of the tree 
+would be log(N). Therefore, the space complexity in this case would be O(log(N)).
+*/
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        Deque<TreeNode> s = new LinkedList<TreeNode>();
+        Deque<Integer> val = new LinkedList<Integer>();
+        if (root == null) {
+            return false;
+        }
+        s.push(root);
+        val.push(sum);
+        while (!s.isEmpty()) {
+            TreeNode cur = s.pop();
+            int curVal = val.pop();
+            // 找到满足条件的leave
+            if (cur.val == curVal && cur.left == null && cur.right == null) {
+                return true;
+            }
+            if (cur.left != null) {
+                s.push(cur.left);
+                val.push(curVal - cur.val);
+            }
+            if (cur.right != null) {
+                s.push(cur.right);
+                val.push(curVal - cur.val);
+            }
+        }
+        return false;
+    }
+}
